d56aeb2caf3c4e41c65eb99eb04e5d3ac163461d
Mykola Mysko <mykola.mysko@gmail.com>
2019-02-01 19:41:37 +0200

    saving before modifying config for css classes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import JSON from './db.json';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//Components
+import Header from './components/Header';
+import NewsList from './components/News_list'
+
+
+class App extends Component {
+
+  state = {
+    news: JSON
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <NewsList news={this.state.news} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
